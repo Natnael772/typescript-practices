55e2ef1b0dc60dd8bf09ff719f8614cfc5dc80df
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-05-21 22:32:44 +0300

    Checking for arrays

--- demo.js
+++ demo.js
@@ -2,8 +2,9 @@ var inputs = document.querySelector("input");
 var x;
 x = "me";
 //checking change in data types
-x = 12;
 console.log(x);
 var area = function (diameter) {
   return diameter * Math.PI;
 };
+//Checking
+console.log(area("yo"));
--- demo.ts
+++ demo.ts
@@ -3,7 +3,6 @@ let x: string;
 x = "me";
 
 //checking change in data types
-x = 12;
 console.log(x);
 
 const area = (diameter: number) => {
@@ -12,3 +11,14 @@ const area = (diameter: number) => {
 
 //Checking
 console.log(area("yo"));
+
+//array
+let names = ["nati", "miki", "dani"];
+console.log(names);
+
+names.push(4); //error
+console.log(names);
+
+let numbers = [2, 4, 5, 6];
+numbers.push("str"); //error
+console.log(numbers);
