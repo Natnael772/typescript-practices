96b09b668fcebc6717402d1e3726bde366319f84
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-05-26 14:15:24 +0300

    strict : true

--- public/strictness.js
+++ public/strictness.js
@@ -1,3 +1,3 @@
 "use strict";
 //strict:true
-//setting "strict": true means that you are enabling all strict type-checking options provided by TypeScript. It is a shorthand way of enabling several individual strictness flags.
+//setting "strict": true means  you are enabling all strict type-checking options provided by TypeScript. It is a shorthand way of enabling several individual strictness flags.
