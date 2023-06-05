452ee17cb454afdd83089d17e5d1091292b71afa
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-06-05 19:38:56 +0300

    Objects in ts

---
+++ public/objects.js
@@ -0,0 +1,11 @@
+"use strict";
+//objects
+function greet1(person) {
+    console.log(`Hello ${person.name}`);
+}
+function greet2(person) {
+    console.log(`Hello ${person.name}`);
+}
+function greet3(person) {
+    console.log(`Hello ${person.name}`);
+}
