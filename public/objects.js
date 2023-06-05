0f4ee68e0cbad9c37880a310b5f00e6b0d700a6a
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-06-05 23:00:58 +0300

    inheritance

--- public/objects.js
+++ public/objects.js
@@ -9,3 +9,13 @@ function greet2(person) {
 function greet3(person) {
     console.log(`Hello ${person.name}`);
 }
+//object destructuring
+function paintShape({ xPos = 0, yPos = 0 }) {
+    console.log(`x coordinate at ${xPos}`);
+    console.log(`y coordinate at ${yPos}`);
+}
+const signature1 = {
+    name: "str",
+    length: 4,
+};
+console.log(signature1);
