9d3b30061cc79ac4abf7b166a52efcf2194abdaf
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-06-07 18:48:14 +0300

    strict initialization

---
+++ public/class.js
@@ -0,0 +1,7 @@
+"use strict";
+class Pointt {
+}
+// --strictPropertyInitialization setting controls if class fields need to be initialized in constructor
+const pt1 = new Pointt();
+pt1.x = 0;
+pt1.y = 0;
