ec9123c7f4af8f083b6063d5e5a9c565640fae4d
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-06-07 12:13:11 +0300

    public private protected

--- public/modifier.js
+++ public/modifier.js
@@ -7,18 +7,40 @@ class Animall {
         this.age = age;
         this.breed = breed;
     }
+    //private methods are not directly visible or accessible to objects of the class in which they are defined. Private methods can only be accessed and called within the class itself, not from external code or objects.
+    privateMethod() {
+        console.log("Private method called");
+    }
+    //public methods are accessible everywhere
+    publicMethod() {
+        console.log("Animal");
+        this.privateMethod();
+    }
     getInfo() {
         return `${this.name} is ${this.age} yeares old`;
     }
     getAge() {
         return this.age;
     }
+    //Protected methods can be accessed and invoked within the class itself as well as in its subclasses.
+    protectedMethod() {
+        console.log("protected method");
+    }
     getBreed() {
         return this.breed;
     }
-    static getClassName() {
-        return "Animal";
+    //static method
+    //Static methods can be called directly on the class without creating an instance of the class.
+    static staticMethod() {
+        console.log("Static method");
     }
 }
 const myAnimal = new Animall("Cat", 10, "Eth");
 console.log(myAnimal);
+console.log(myAnimal.getInfo());
+myAnimal.publicMethod();
+class Cat extends Animal {
+    makeCatSound() {
+        this.makeCatSound();
+    }
+}
