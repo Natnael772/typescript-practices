994a19c56395f5403197c55681420ee7d45bfb0c
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-06-07 19:05:36 +0300

    Inheritance and super method

--- public/class.js
+++ public/class.js
@@ -1,7 +1,26 @@
 "use strict";
 class Pointt {
+    // COnstructor
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
 }
 // --strictPropertyInitialization setting controls if class fields need to be initialized in constructor
 const pt1 = new Pointt();
 pt1.x = 0;
 pt1.y = 0;
+//inheritance and super method
+class Electronics {
+    constructor(name, imei) {
+        this.name = name;
+        this.imei = imei;
+        console.log(`Parent class`);
+    }
+}
+class Smartphone extends Electronics {
+    constructor(name, imei) {
+        super(name, imei);
+        console.log("Child class");
+    }
+}
